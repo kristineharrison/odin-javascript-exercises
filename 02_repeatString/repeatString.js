39a6b35b98487b0e4c1dbe0dcc6bef6fb39eb4ce
Kristine Harrison <hello@kristine.works>
2025-03-09 12:29:40 -0400

    Test 02 solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (word, number) {
+  if (number < 0) return "ERROR";
+  let string = "";
+  for (let i = 0; i < number; i++) {
+    string += word;
+  }
+  return string;
 };
-
+repeatString();
+console.log(repeatString);
 // Do not edit below this line
 module.exports = repeatString;
